fa4cc091851b48389a05239aa9e99a2f00edac94
SmythyCosta <smythy.costa@hotmail.com>
2019-08-17 10:18:12 -0300

    project base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-tes'`, () => {
+  it(`should have as title 'angular8app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app-tes');
+    expect(app.title).toEqual('angular8app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-tes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular8app!');
   });
 });
